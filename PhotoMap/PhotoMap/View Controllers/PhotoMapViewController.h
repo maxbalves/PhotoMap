bb90fb127000e38dde587a224bad8c91a7dac657
Max Bagatini Alves <maxbagatini@hotmail.com>
2022-06-30 09:08:38 -0700

    Finished lab

---
+++ PhotoMap/PhotoMap/View Controllers/PhotoMapViewController.h
@@ -0,0 +1,17 @@
+//
+//  PhotoMapViewController.h
+//  PhotoMap
+//
+//  Created by emersonmalca on 7/8/18.
+//  Copyright © 2018 Codepath. All rights reserved.
+//
+
+// View Controllers
+#import "LocationsViewController.h"
+
+// Frameworks
+#import <UIKit/UIKit.h>
+
+@interface PhotoMapViewController : UIViewController
+
+@end
